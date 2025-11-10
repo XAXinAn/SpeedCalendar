e37cf7d3230386b016d866425c1534fdfb79c1ab
Jiang Hangxi <jianghangximail@gmail.com>
2025-11-10 12:54:07 +0800

    模拟器可以显示启动屏但是实机不可以

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.navigation:navigation-compose:2.9.6")
+    implementation("androidx.core:core-splashscreen:1.2.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/speedcalendar/MainActivity.kt
+++ app/src/main/java/com/example/speedcalendar/MainActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.speedcalendar.ui.MainScreen
 import com.example.speedcalendar.ui.theme.SpeedCalendarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 1. 在 super.onCreate 之前调用
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             SpeedCalendarTheme {
