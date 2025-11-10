67c8b3cf94b835e4aaf7daf5e3dbe269f227247c
Jiang Hangxi <jianghangximail@gmail.com>
2025-11-10 20:47:08 +0800

    修改了主题色

--- app/src/main/java/com/example/speedcalendar/ui/theme/Theme.kt
+++ app/src/main/java/com/example/speedcalendar/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.speedcalendar.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,13 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = AppPrimaryColor, // 使用我们定义的浅蓝色
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = AppPrimaryColor, // 使用我们定义的浅蓝色
     secondary = PurpleGrey40,
     tertiary = Pink40
 
@@ -37,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 fun SpeedCalendarTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false, // 默认关闭动态颜色，以确保主题统一
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
